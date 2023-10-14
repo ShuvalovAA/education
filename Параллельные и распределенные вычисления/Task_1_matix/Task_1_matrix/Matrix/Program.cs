a825b3489b698ddeda2275975187b8d09fda30df
Artemiy_Shuvalov <a.shuvalov@sailplay.ru>
2023-10-14 16:23:14 +0300

    Умножение матриц
    exe по адресу Task_1_matix\Task_1_matrix\Matrix\bin\Debug\Matrix.exe

---
+++ Параллельные и распределенные вычисления/Task_1_matix/Task_1_matrix/Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MConsole mConsole = new MConsole();
+
+            int a = 0;
+            mConsole.GetInteger("размер матрицы:", ref a);
+            if (a>0)
+            {
+                Math math = new Math(a);
+                int[,] matrixResult = math.MatrixMult(math.matrix1, math.matrix2);
+
+                math.PrintMatrix(math.matrix1, "\nматрица 1");
+                math.PrintMatrix(math.matrix2, "\nматрица 2");
+                math.PrintMatrix(matrixResult, "\nпроизведение матрицы 1, 2");
+
+                Console.WriteLine($"\nвремя выполненияd: {math.timeElapsed}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Размер матрицы нужен больше 0.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
