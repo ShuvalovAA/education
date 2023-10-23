909df2b251b578262c65fcafd737e3fda28cc3a3
Artemiy_Shuvalov <a.shuvalov@sailplay.ru>
2023-10-23 13:57:13 +0300

    веб технологии

--- Веб-технологии/wb/reg_per/models.py
+++ Веб-технологии/wb/reg_per/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class VisiterForum(models.Model):
+    id = models.fields.AutoField(primary_key=True)
+    first_name = models.TextField(null=False)
+    last_name = models.TextField(null=False)
+    middle_name = models.TextField(null=False)
+    birth_date = models.DateField(null=True)
+    is_report = models.BooleanField()
+    report_theme = models.TextField(null=True)
+    phone = models.TextField(null=False)
+    email = models.TextField(null=False)
--- Веб-технологии/wb/reg_per/views.py
+++ Веб-технологии/wb/reg_per/views.py
@@ -1,5 +1,11 @@
+import json
+
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+
+from reg_per.models import VisiterForum
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 
@@ -7,8 +13,30 @@ class HomePageView(TemplateView):
     template_name = "my_index.html"
 
 
-def reg_save():
-    return 1
+def reg_save(request):
 
-def reg_list():
-    return 1
+    body = json.loads(request.body)
+    first_name = body.get('first_name')
+    last_name = body.get('last_name')
+    middle_name = body.get('middle_name')
+    birth_date = body.get('birth_date')
+    is_report = True if body.get('is_report') == 'on' else False
+    phone = body.get('phone')
+    email = body.get('email')
+    visitor = {
+    "first_name": first_name,
+    "last_name": last_name,
+    "middle_name": middle_name,
+    "birth_date": birth_date,
+    "is_report": is_report,
+    "phone": phone,
+    "email": email,
+
+    }
+
+    VisiterForum.objects.create(**visitor)
+    return JsonResponse({"status":"ok"})
+
+def reg_list(request):
+    all_visit = serializers.serialize("json", VisiterForum.objects.all())
+    return JsonResponse({"list":all_visit})
