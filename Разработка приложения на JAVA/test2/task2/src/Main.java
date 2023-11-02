513115a008917f2f290ff6c54e2883907dcc2cb8
Artemiy_Shuvalov <56578819+ArtemiyShuvalov@users.noreply.github.com>
2023-11-02 12:32:09 +0300

    tasks adding

---
+++ Разработка приложения на JAVA/test2/task2/src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+
+
+        class UserFrame extends JFrame{
+
+            public UserFrame(){
+
+                setSize(500, 300);
+
+                setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+            }
+
+        }
+        }
+    }
